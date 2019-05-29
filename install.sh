23d2426e845277e5dfe0278d27c36a964c4b7c7a
Yusuke Ohi <yohi.consadole12@gmail.com>
2019-05-29 03:52:45 +0000

    add fish

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+$(mkdir -p ~/.config)
+
+# VIM
 $(ln -nfs ~/dotfiles/vim ~/.vim)
 $(ln -nfs ~/dotfiles/vim/rc/vimrc ~/.vimrc)
 $(ln -nfs ~/dotfiles/vim/rc/gvimrc ~/.gvimrc)
 
-$(mkdir -p ~/.config)
 $(ln -nfs ~/dotfiles/vim ~/.config/nvim)
 $(ln -nfs ~/dotfiles/vim/rc/vimrc ~/.config/nvim/init.vim)
 
 rm -rf ~/.yarn/
 curl -sL install-node.now.sh/lts | /bin/bash
 
-
+# ZSH
 $(ln -nfs ~/dotfiles/zsh/rc/zshrc ~/.zshrc)
+
+# FISH
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+$(ln -nfs ~/dotfiles/fish/rc/fishrc ~/.config/fish/config.fish)
