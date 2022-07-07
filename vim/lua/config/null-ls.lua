fffcae1267edfc3d7e83486ab1dfc1afe9264d60
Yusuke Ohi <ohi@diamondhead.tech>
2022-07-07 14:25:54 +0900

    commit

--- vim/lua/config/null-ls.lua
+++ vim/lua/config/null-ls.lua
@@ -1,34 +1,40 @@
 -- null-ls --
 local null_ls = require("null-ls")
+local null_ls_utils = require("null-ls.utils")
+local root_dir = vim.env.PYTHONPATH
 -- print(vim.inspect(vim))
 local sources = {
-    debug = true,
+    -- debug = true,
     null_ls.builtins.diagnostics.djlint.with({
         --extra_args = { "--config", "./.flake8" },
+        prefer_local = root_dir .. ".venv/bin",
         extra_args = function(params)
             return  {
                 '--config',
-                params.root .. '/django/project/.djlintrc'
+                params.root .. '/.djlintrc'
             }
         end,
 
     }),
     null_ls.builtins.diagnostics.cspell.with({
         --extra_args = { "--config", "./.flake8" },
+        prefer_local = root_dir .. ".venv/bin",
         extra_args = function(params)
             return  {
                 '--config',
-                params.root .. '/django/project/.cspell'
+                params.root .. '/.cspell'
             }
         end,
 
     }),
     null_ls.builtins.diagnostics.mypy.with({
         --extra_args = { "--config", "./.flake8" },
+        prefer_local = root_dir .. ".venv/bin",
+        filetypes = { '*' },
         extra_args = function(params)
             return  {
                 '--config',
-                params.root .. '/django/project/.mypy.ini'
+                params.root .. '/.mypy.ini'
             }
         end,
 
@@ -36,10 +42,11 @@ local sources = {
     null_ls.builtins.diagnostics.flake8.with(
         {
             --extra_args = { "--config", "./.flake8" },
+            prefer_local = root_dir .. ".venv/bin",
             extra_args = function(params)
                 return  {
                     '--config',
-                    params.root .. '/django/project/.flake8'
+                    params.root .. '/.flake8'
                 }
             end,
         }
@@ -47,9 +54,11 @@ local sources = {
 }
 
 null_ls.setup({
+    debug = true,
     -- diagnostics_format = '#{m} [#{c}]',
     -- diagnostics_format = '#{m} (#{s})',
     diagnostics_format = '#{s}: #{m}',
     sources = sources,
+    root_dir = null_ls_utils.root_pattern('.venv'),
     -- on_attach = on_attach
 })
