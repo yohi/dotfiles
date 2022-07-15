47355cf84d3feb44c2b20a266295f61b9facb2e1
Yusuke Ohi <ohi@diamondhead.tech>
2022-07-15 20:03:51 +0900

    commit

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ホームディレクトリを英語名にする
-#LANG=C xdg-user-dirs-gtk-updat
+LANG=C xdg-user-dirs-gtk-updat
 
 sudo apt update
 sudo apt -y upgrade
@@ -9,23 +9,22 @@ sudo apt -y upgrade
 sudo apt -y install language-pack-ja
 sudo update-locale LANG=ja_JP.UTF8
 
+# HOMEBREW
 sudo apt -y install build-essential curl file git
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/y_ohi/.profile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew bundle
 
+
 sudo apt -y install chrome-gnome-shell
 
 $(mkdir -p ~/.config)
 
 # VIM
-pip3 install -y pynvim
 $(ln -nfs ~/dotfiles/vim ~/.vim)
 $(ln -nfs ~/dotfiles/vim/rc/vimrc ~/.vimrc)
 $(ln -nfs ~/dotfiles/vim/rc/gvimrc ~/.gvimrc)
-
-
 $(ln -nfs ~/dotfiles/vim ~/.config/nvim)
 $(ln -nfs ~/dotfiles/vim/rc/vimrc ~/.config/nvim/init.vim)
 
@@ -57,7 +56,7 @@ cat <<EOF | sudo sh -x
 apt-get install -y uidmap
 EOF
 systemctl --user start docker.service
-sudo loginctl enable-linger y_ohi   # TODO USERNAME
+sudo loginctl enable-linger ${USER}
 
 # git config
 git config --global user.name 'Yusuke Ohi'
@@ -78,7 +77,7 @@ sudo apt update && sudo apt install -y terminator
 sudo snap install -y postman
 
 # TablePlus
-wget -O - -q http://deb.tableplus.com/apt.tableplus.com.gpg.key | sudo apt-key add - 
+wget -O - -q http://deb.tableplus.com/apt.tableplus.com.gpg.key | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://deb.tableplus.com/debian tableplus main"
 sudo apt update && sudo apt install -y tableplus
 
@@ -92,16 +91,19 @@ sudo howdy add
 wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-key add -
 wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add -
 # sudo wget https://www.ubuntulinux.jp/sources.list.d/focal.list -O /etc/apt/sources.list.d/ubuntu-ja.list # 20.04
-sudo wget https://www.ubuntulinux.jp/sources.list.d/impish.list -O /etc/apt/sources.list.d/ubuntu-ja.list # 21.10
+# sudo wget https://www.ubuntulinux.jp/sources.list.d/impish.list -O /etc/apt/sources.list.d/ubuntu-ja.list # 21.10
+sudo wget https://www.ubuntulinux.jp/sources.list.d/jammy.list -O /etc/apt/sources.list.d/ubuntu-ja.list # 22.04
 sudo apt update && sudo apt upgrade -y && sudo apt install -y ubuntu-defaults-ja
 
 # CapsLock -> Ctrl
 setxkbmap -option "ctrl:nocaps"
 sudo update-initramfs -u
 
+# tilix
+sudo apt install tilix
+dconf load /com/gexperts/Tilix/ < tilix/tilix.dconf
+
+
 
 
 # https://qiita.com/harmegiddo/items/0daac48c0f58596a52f1
-
-# sudo add-apt-repository ppa:aslatter/ppa
-# sudo apt install alacritty
