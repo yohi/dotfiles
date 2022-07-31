6231888d9ec00ce348a5fc56e7862e445c9aa0cf
Yusuke Ohi <yohi.consadole12@gmail.com>
2022-08-01 08:14:12 +0900

    commit

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -1,8 +1,126 @@
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+-- local mason_lspconfig = require('mason-lspconfig')
+-- 
+-- mason_lspconfig.setup({
+--     ensure_installed = {
+--         'bash-language-server',
+--         'cspell',
+--         'djlint',
+--         'dockerfile-language-server',
+--         'dot-language-server',
+--         'flake8',
+--         'html-lsp',
+--         'isort',
+--         'json-lsp',
+--         'json-to-struct',
+--         'lua-language-server',
+--         'luacheck',
+--         'luaformatter',
+--         'markdownlint',
+--         'mypy',
+--         'pyright',
+--         'shellcheck',
+--         'sql-formatter',
+--         'sqlls',
+--         'vim-language-server',
+--         'yaml-language-server',
+--         'yamllint',
+--     },
+--     automatic_installation = true,
+-- })
+-- 
+-- local lspconfig = require('lspconfig')
+-- mason_lspconfig.setup_handlers({
+--     function(server_name)
+--         local opts = {}
+-- 
+--         ---@diagnostic disable: undefined-global
+--         if lsp_capabilities ~= nil then
+--             opts.capabilities = lsp_capabilities
+--         end
+--         ---@diagnostic enable: undefined-global
+-- 
+--         -- serverに対応しているfiletypeのbufferを開いたら、
+--         if server_name == 'sumneko_lua' then
+--             print('hello world')
+--         elseif server_name == 'tsserver' or server_name == 'eslint' then
+--             opts.root_dir = lspconfig.util.root_pattern("package.json")
+--         elseif server_name == 'denols' then
+--             opts.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts")
+--             opts.init_options = { lint = true, unstable = true, }
+--         elseif server_name == 'pyright' then
+--             print('hello pyright')
+--             opts.root_dir = lspconfig.util.root_pattern(".venv")
+--         end
+-- 
+--         -- 実行するfunctionを設定します。
+--         -- sumneko_luaはluaのLSP serverなので、
+--         -- luaのbufferを開いたら、実行するfunctionです。
+--         opts.on_attach = function(client, bufnr)
+--             print('hello setup')
+--             -- Key Mappings.
+--             -- See `:help vim.lsp.*` for documentation on any of the below functions
+--             local bufopts = { noremap=true, silent=true, buffer=bufnr }
+--             -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+--             vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, bufopts)
+--             -- vim.keymap.set('n', '<Leader>lk', vim.lsp.buf.hover, bufopts)
+--             -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+--             -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+--             -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+--             -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+--             -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+-- 
+--             -- vim.keymap.set('n', '<space>wl', function()
+--             --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--             -- end, bufopts)
+-- 
+--             -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+--             -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+--             -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+--             -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+--             -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+--         end
+-- 
+--         -- LSPのsetupをします。
+--         -- setupをしないとserverは動作しません。
+--         lspconfig[server_name].setup(opts)
+-- 
+--         -- vim.diagnostic.open_float()
+-- 
+--         -- vim.cmd [[ do User LspAttachBuffers ]]
+--     end
+-- })
+-- 
+-- print(lspconfig.util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python'))
+-- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 local signs = {
-    -- { name = "DiagnosticSignError", text = "E" },
-    -- { name = "DiagnosticSignWarn", text = "W" },
-    -- { name = "DiagnosticSignHint", text = "H" },
-    -- { name = "DiagnosticSignInfo", text = "I" },
     { name = "DiagnosticSignError", text = "" },
     { name = "DiagnosticSignWarn", text = "" },
     { name = "DiagnosticSignHint", text = "" },
@@ -15,16 +133,13 @@ for _, sign in ipairs(signs) do
         {
             texthl = sign.name,
             text = sign.text,
-            numhl = ""
+            numhl = "",
         }
     )
 end
 
 local config = {
     virtual_text = false,
-    -- virtual_text = {
-    --     prefix: '',
-    -- },
     signs = {
         active = signs,
     },
@@ -38,11 +153,6 @@ local config = {
         header = "",
         prefix = "",
         border = "rounded",
-        -- border = "double",
-        -- border = "shadow",
-        -- border = "solid",
-        -- border = "none",
-        -- border = "single",
     },
 }
 vim.diagnostic.config(config)
@@ -55,13 +165,16 @@ local function on_cursor_hold()
 end
 
 local diagnostic_hover_augroup_name = "lspconfig-diagnostic"
+
 vim.api.nvim_set_option('updatetime', 500)
+
 vim.api.nvim_create_augroup(
     diagnostic_hover_augroup_name,
     {
         clear = true,
     }
 )
+
 vim.api.nvim_create_autocmd(
     {
         "CursorHold",
@@ -72,12 +185,8 @@ vim.api.nvim_create_autocmd(
     }
 )
 
-
-
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover, {
-        -- Use a sharp border with `FloatBorder` highlights
-        -- border = "single"
         border = "rounded",
     }
 )
@@ -113,29 +222,97 @@ local function lsp_highlight_document(client)
     -- illuminate.on_attach(client)
 end
 
-local function lsp_keymaps(bufnr)
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, bufopts)
-    -- vim.keymap.set('n', '<Leader>lk', vim.lsp.buf.hover, bufopts)
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
 
-    -- vim.keymap.set('n', '<space>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, bufopts)
 
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-end
+-- 2. build-in LSP function
+-- keyboard shortcut
+
+-- カーソル下の変数情報表示
+vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+
+-- 改行やインデントのフォーマティング
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+
+-- 
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+
+--定義ジャンプ
+vim.keymap.set('n', '<F12>', '<cmd>lua vim.lsp.buf.definition()<CR>')
+
+-- 
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+
+-- 
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+
+-- 
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+
+-- カーソル下の変数参照元一覧表示
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+-- 変数名リネーム
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+
+-- 
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+
+-- 
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+
+-- 
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+-- local opts = { capabilities = capabilities, on_attach = on_attach }
+
+local lspconfig = require("lspconfig")
+local mason_lspconfig = require('mason-lspconfig')
+
+require("mason-lspconfig").setup_handlers({
+    function(server_name)
+
+        -- Mappings.
+        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+        local opts = {
+            noremap = true,
+            silent = true,
+        }
+       -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+       -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+       -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+       -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+       -- -- Use an on_attach function to only map the following keys
+       -- -- after the language server attaches to the current buffer
+       -- opts.on_attach = function(client, bufnr)
+       --     -- Enable completion triggered by <c-x><c-o>
+       --     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+       --     -- Mappings.
+       --     -- See `:help vim.lsp.*` for documentation on any of the below functions
+       --     local bufopts = {
+       --         noremap=true,
+       --         silent=true,
+       --         buffer=bufnr,
+       --     }
+       --     vim.keymap.set('n', '<F12>', vim.lsp.buf.declaration, bufopts)
+       --     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+       --     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+       --     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+       --     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+       --     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+       --     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+       --     vim.keymap.set('n', '<space>wl', function()
+       --       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+       --     end, bufopts)
+       --     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+       --     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+       --     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+       --     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+       --     vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+       --     require("nvim-navic").attach(client, bufnr)
+       -- end
 
 
 -- installしているserverの起動準備をします。
@@ -158,18 +335,14 @@ lsp_installer.on_server_ready(function(server)
         -- print(server.name)
         if server.name == 'sumneko_lua' then
             print('hello world')
-        elseif server.name == 'tsserver' or server.name == 'eslint' then
-            opts.root_dir = lspconfig.util.root_pattern("package.json")
-        elseif server.name == 'denols' then
-            opts.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts")
-            opts.init_options = { lint = true, unstable = true, }
-        elseif server.name == 'pyright' then
+        elseif server_name == 'pyright' then
             print('hello pyright')
             opts.root_dir = lspconfig.util.root_pattern(".venv")
         end
 
-
-    end
+        -- LSPのsetupをします。
+        -- setupをしないとserverは動作しません。
+        lspconfig[server_name].setup(opts)
 
     -- LSPのsetupをします。
     -- setupをしないとserverは動作しません。
@@ -177,55 +350,17 @@ lsp_installer.on_server_ready(function(server)
 
     -- vim.diagnostic.open_float()
 
-    vim.cmd [[ do User LspAttachBuffers ]]
+local handle_lsp = function(opts)
+    return opts
+end
 
-end)
-
-
-
-
-
-
--- local function lsp_keymaps(bufnr)
---     local opts = {
---         noremap = true,
---         silent = true
---     }
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
---     -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
---     -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
---     -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
---     vim.api.nvim_buf_set_keymap(
---         bufnr,
---         "n",
---         "gl",
---         '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
---         opts
---     )
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
---     vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
---     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
--- end
-
-
-
-
--- lspconfig --
-
-local lspconfig = require('lspconfig')
-
-lspconfig.pyright.setup {
+lspconfig.pyright.setup(handle_lsp {
+    root_dir = lspconfig.util.root_pattern('.venv'),
     settings = {
         python = {
             analysis = {
-                -- disableLanguageService = false,
-                -- disableOrganizeImports = false,
+                -- disableLanguageService = true,
+                -- disableOrganizeImports = true,
                 -- openFilesOnly = false,
                 -- useLibraryCodeForType = false
                 autoImportCompletions = true,
@@ -235,12 +370,16 @@ lspconfig.pyright.setup {
                 -- extraPaths = '',
                 logLevel = 'Information',
                 -- stubPath = '',
-                typeCheckingMode = 'strict',
+                typeCheckingMode = 'off',
                 -- typeshedPaths = '',
                 useLibraryCodeForType = true,
             },
-            -- pythonPath = ''
-            -- venvPath = ''
+            pythonPath = lspconfig.util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python'),
+            venvPath = '.',
+            venv='.venv'
         },
     },
-}
+    -- before_init = function(_, config)
+    --     config.settings.python.venvPath = lspconfig.util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+    -- end
+})
--- vim/lua/config/null-ls.lua
+++ vim/lua/config/null-ls.lua
@@ -6,43 +6,32 @@ local function dump(o)
          s = s .. '['..k..'] = ' .. dump(v) .. ','
       end
       return s .. '} '
-   else
+  else
       return tostring(o)
    end
 end
 
 -- null-ls --
 local null_ls = require("null-ls")
-local null_ls_utils = require("null-ls.utils")
-local root_dir = vim.env.PYTHONPATH
--- print(vim.inspect(vim))
-local sources = {
-    -- debug = true,
-    null_ls.builtins.diagnostics.djlint.with({
-        --extra_args = { "--config", "./.flake8" },
-        prefer_local = root_dir .. ".venv/bin",
-        -- extra_args = function(params)
-        --     return  {
-        --         '--config',
-        --         params.root .. '/.djlintrc'
-        --     }
-        -- end,
-    }),
-    null_ls.builtins.diagnostics.cspell.with({
-        --extra_args = { "--config", "./.flake8" },
-        prefer_local = root_dir .. ".venv/bin",
-        diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity =  vim.diagnostic.severity["INFO"]
-        end,
-        -- extra_args = function(params)
-        --     return  {
-        --         '--config',
-        --         params.root .. '/.cspell'
-        --     }
-        -- end,
+local utils = require("null-ls.utils")
+local command_resolver = require("null-ls.helpers.command_resolver")
+local root_dir = utils.root_pattern('.venv')
 
-    }),
+local sources = {
+    debug = true,
+    -- null_ls.builtins.diagnostics.flake8.with({
+    --     prefer_local = '.venv/bin',
+    --         --extra_args = { "--config", "./.flake8" },
+    --         -- prefer_local = root_dir .. ".venv/bin",
+    --         -- extra_args = function(params)
+    --         --     return  {
+    --         --         '--config',
+    --         --         params.root .. '/.flake8'
+    --         --     }
+    --         -- end,
+    -- }),
     null_ls.builtins.diagnostics.mypy.with({
+        prefer_local = '.venv/bin',
         --extra_args = { "--config", "./.flake8" },
         prefer_local = root_dir .. ".venv/bin",
         -- filetypes = { '*' },
@@ -52,57 +41,57 @@ local sources = {
         --         params.root .. '/.mypy.ini'
         --     }
         -- end,
-
     }),
-    null_ls.builtins.diagnostics.flake8.with(
-        {
-            --extra_args = { "--config", "./.flake8" },
-            prefer_local = root_dir .. ".venv/bin",
-            -- extra_args = function(params)
-            --     return  {
-            --         '--config',
-            --         params.root .. '/.flake8'
-            --     }
-            -- end,
-        }
-    ),
-    null_ls.builtins.formatting.djhtml.with(
-        {
-            --extra_args = { "--config", "./.flake8" },
-            prefer_local = root_dir .. ".venv/bin",
-            -- extra_args = function(params)
-            --     return  {
-            --         '--config',
-            --         params.root .. '/.flake8'
-            --     }
-            -- end,
-        }
-    ),
-    null_ls.builtins.formatting.djlint.with(
-        {
-            --extra_args = { "--config", "./.flake8" },
-            prefer_local = root_dir .. ".venv/bin",
-            -- extra_args = function(params)
-            --     return  {
-            --         '--config',
-            --         params.root .. '/.flake8'
-            --     }
-            -- end,
-        }
-    ),
-    null_ls.builtins.formatting.isort.with(
-        {
-            --extra_args = { "--config", "./.flake8" },
-            prefer_local = root_dir .. ".venv/bin",
-            -- extra_args = function(params)
-            --     return  {
-            --         '--config',
-            --         params.root .. '/.flake8'
-            --     }
-            -- end,
-        }
-    ),
-
+    -- null_ls.builtins.formatting.isort.with({
+    --     prefer_local = '.venv/bin',
+    --         --extra_args = { "--config", "./.flake8" },
+    --         -- prefer_local = root_dir .. ".venv/bin",
+    --         -- extra_args = function(params)
+    --         --     return  {
+    --         --         '--config',
+    --         --         params.root .. '/.flake8'
+    --         --     }
+    --         -- end,
+    -- }),
+    -- null_ls.builtins.formatting.djhtml.with({
+    --     prefer_local = '.venv/bin',
+    --         -- extra_args = { "--config", "./.flake8" },
+    --         -- prefer_local = root_dir .. ".venv/bin",
+    --         -- extra_args = function(params)
+    --         --     return  {
+    --         --         '--config',
+    --         --         params.root .. '/.flake8'
+    --         --     }
+    --         -- end,
+    -- }),
+    -- null_ls.builtins.diagnostics.djlint.with({
+    --     prefer_local = '.venv/bin',
+    --     --extra_args = { "--config", "./.flake8" },
+    --     -- prefer_local = root_dir .. ".venv/bin",
+    --     -- extra_args = function(params)
+    --     --     return  {
+    --     --         '--config',
+    --     --         params.root .. '/.djlintrc'
+    --     --     }
+    --     -- end,
+    -- }),
+    -- null_ls.builtins.diagnostics.cspell.with({
+    --     --extra_args = { "--config", "./.flake8" },
+    --     -- prefer_local = root_dir .. ".venv/bin",
+    --     diagnostics_postprocess = function(diagnostic)
+    --         diagnostic.severity =  vim.diagnostic.severity["INFO"]
+    --     end,
+    --     extra_args = {
+    --         '--config',
+    --         cspell_files.config,
+    --     }
+    --     -- extra_args = function(params)
+    --     --     return  {
+    --     --         '--config',
+    --     --         params.root .. '/.cspell'
+    --     --     }
+    --     -- end,
+    -- }),
 }
 
 -- print('sources')
@@ -115,6 +104,7 @@ local sources = {
 --     end
 --     print('========================')
 -- end
+local root_dir = utils.root_pattern('.venv')
 null_ls.setup({
     debug = true,
     -- diagnostics_format = '#{m} [#{c}]',
@@ -122,7 +112,7 @@ null_ls.setup({
     diagnostics_format = '#{s}: #{m}',
     sources = sources,
     -- sources = { null_ls.builtins.diagnostics.mypy },
-    root_dir = null_ls_utils.root_pattern('.venv'),
+    root_dir = utils.root_pattern('.venv'),
     -- on_attach = on_attach
     -- on_attach = function(_, bufnr)
     --     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca',
