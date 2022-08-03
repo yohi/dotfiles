df9c770aaf19919b49cb0710d42907a9f081bd00
Yusuke Ohi <yohi@diamondhead.tech>
2022-08-03 14:09:31 +0900

    修正

--- vim/lua/config/null-ls.lua
+++ vim/lua/config/null-ls.lua
@@ -19,17 +19,17 @@ local root_dir = utils.root_pattern('.venv')
 
 local sources = {
     debug = true,
-    -- null_ls.builtins.diagnostics.flake8.with({
-    --     prefer_local = '.venv/bin',
-    --         --extra_args = { "--config", "./.flake8" },
-    --         -- prefer_local = root_dir .. ".venv/bin",
-    --         -- extra_args = function(params)
-    --         --     return  {
-    --         --         '--config',
-    --         --         params.root .. '/.flake8'
-    --         --     }
-    --         -- end,
-    -- }),
+    null_ls.builtins.diagnostics.flake8.with({
+        prefer_local = '.venv/bin',
+            --extra_args = { "--config", "./.flake8" },
+            -- prefer_local = root_dir .. ".venv/bin",
+            -- extra_args = function(params)
+            --     return  {
+            --         '--config',
+            --         params.root .. '/.flake8'
+            --     }
+            -- end,
+    }),
     null_ls.builtins.diagnostics.mypy.with({
         prefer_local = '.venv/bin',
         --extra_args = { "--config", "./.flake8" },
@@ -41,56 +41,52 @@ local sources = {
         --     }
         -- end,
     }),
-    -- null_ls.builtins.formatting.isort.with({
-    --     prefer_local = '.venv/bin',
-    --         --extra_args = { "--config", "./.flake8" },
-    --         -- prefer_local = root_dir .. ".venv/bin",
-    --         -- extra_args = function(params)
-    --         --     return  {
-    --         --         '--config',
-    --         --         params.root .. '/.flake8'
-    --         --     }
-    --         -- end,
-    -- }),
-    -- null_ls.builtins.formatting.djhtml.with({
-    --     prefer_local = '.venv/bin',
-    --         -- extra_args = { "--config", "./.flake8" },
-    --         -- prefer_local = root_dir .. ".venv/bin",
-    --         -- extra_args = function(params)
-    --         --     return  {
-    --         --         '--config',
-    --         --         params.root .. '/.flake8'
-    --         --     }
-    --         -- end,
-    -- }),
-    -- null_ls.builtins.diagnostics.djlint.with({
-    --     prefer_local = '.venv/bin',
-    --     --extra_args = { "--config", "./.flake8" },
-    --     -- prefer_local = root_dir .. ".venv/bin",
-    --     -- extra_args = function(params)
-    --     --     return  {
-    --     --         '--config',
-    --     --         params.root .. '/.djlintrc'
-    --     --     }
-    --     -- end,
-    -- }),
-    -- null_ls.builtins.diagnostics.cspell.with({
-    --     --extra_args = { "--config", "./.flake8" },
-    --     -- prefer_local = root_dir .. ".venv/bin",
-    --     diagnostics_postprocess = function(diagnostic)
-    --         diagnostic.severity =  vim.diagnostic.severity["INFO"]
-    --     end,
-    --     extra_args = {
-    --         '--config',
-    --         cspell_files.config,
-    --     }
-    --     -- extra_args = function(params)
-    --     --     return  {
-    --     --         '--config',
-    --     --         params.root .. '/.cspell'
-    --     --     }
-    --     -- end,
-    -- }),
+    null_ls.builtins.formatting.isort.with({
+        prefer_local = '.venv/bin',
+            --extra_args = { "--config", "./.flake8" },
+            -- prefer_local = root_dir .. ".venv/bin",
+            -- extra_args = function(params)
+            --     return  {
+            --         '--config',
+            --         params.root .. '/.flake8'
+            --     }
+            -- end,
+    }),
+    null_ls.builtins.formatting.djhtml.with({
+        prefer_local = '.venv/bin',
+            -- extra_args = { "--config", "./.flake8" },
+            -- prefer_local = root_dir .. ".venv/bin",
+            -- extra_args = function(params)
+            --     return  {
+            --         '--config',
+            --         params.root .. '/.flake8'
+            --     }
+            -- end,
+    }),
+    null_ls.builtins.diagnostics.djlint.with({
+        prefer_local = '.venv/bin',
+        --extra_args = { "--config", "./.flake8" },
+        -- prefer_local = root_dir .. ".venv/bin",
+        -- extra_args = function(params)
+        --     return  {
+        --         '--config',
+        --         params.root .. '/.djlintrc'
+        --     }
+        -- end,
+    }),
+    null_ls.builtins.diagnostics.cspell.with({
+        --extra_args = { "--config", "./.flake8" },
+        -- prefer_local = root_dir .. ".venv/bin",
+        diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity =  vim.diagnostic.severity["INFO"]
+        end,
+        -- extra_args = function(params)
+        --     return  {
+        --         '--config',
+        --         params.root .. '/.cspell'
+        --     }
+        -- end,
+    }),
 }
 
 -- print('sources')
