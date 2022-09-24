d0aa355e048d8de61c659be5db33f5e2bb3600c2
Yusuke Ohi <yohi.consadole12@gmail.com>
2022-09-24 12:44:41 +0900

    commit

--- vim/lua/config/null-ls.lua
+++ vim/lua/config/null-ls.lua
@@ -26,74 +26,56 @@ local root_dir = utils.root_pattern('.venv')
 local sources = {
     debug = true,
     -- Diagnostics
-    -- null_ls.builtins.diagnostics.flake8.with({
-    --     command = 'python3 -m flake8',
-    --     prefer_local = '.venv/bin',
-    --     -- diagnostics_postprocess = function(diagnostic)
-    --     --     -- レベルをWARNに変更
-    --     --     diagnostic.severity =  vim.diagnostic.severity["WARN"]
-    --     -- end,
-    -- }),
+    null_ls.builtins.diagnostics.flake8.with({
+        prefer_local = '.venv/bin',
+        diagnostics_postprocess = function(diagnostic)
+            -- レベルをWARNに変更
+            diagnostic.severity =  vim.diagnostic.severity["WARN"]
+        end,
+    }),
     null_ls.builtins.diagnostics.mypy.with({
         prefer_local = '.venv/bin',
-        -- command = '.venv/bin/mypy',
+        timeout = 60000,
         -- method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-        -- diagnostics_postprocess = function(diagnostic)
-        --     -- レベルをWARNに変更
-        --     diagnostic.severity =  vim.diagnostic.severity["WARN"]
-        -- end,
-        -- cwd = function(params)
-        --     -- falls back to root if return value is nil
-        --     print(params.root)
-        --     print(dump(params))
-        --     return params.root
-        -- end,
-        --cwd = root_dir,
-        -- runtime_condition = function(params)
-        --     print(params.root)
-        --     print(dump(params))
-        --     return params.root:match("my-monorepo-subdir") ~= nil
-        -- end,
-        -- condition = function(params)
-        --     print(dump(params))
-        --     params.root_has_file({'.venv'})
-        --     return params.root_has_file({ ".venv" })
-        -- end,
-        -- extra_args = {
-        --     "--cache-dir",
-        --     "/dev/null",
-        -- }
+        diagnostics_postprocess = function(diagnostic)
+            -- レベルをWARNに変更
+            diagnostic.severity =  vim.diagnostic.severity["WARN"]
+        end,
+        extra_args = {
+            "--cache-dir",
+            "/dev/null",
+        }
     }),
-    -- null_ls.builtins.diagnostics.cspell.with({
-    --     -- extra_args = {
-    --     --     '--config',
-    --     --     '~/.config/cspell/cspell.json',
-    --     -- },
-    --     diagnostics_postprocess = function(diagnostic)
-    --         -- レベルをINFOに変更
-    --         diagnostic.severity =  vim.diagnostic.severity["INFO"]
-    --     end,
-    --     -- condition = function()
-    --     --     -- cpellが実行できるときのみ実行
-    --     --     return vim.fn.executable('cspell') > 0
-    --     -- end,
-    -- }),
+    null_ls.builtins.diagnostics.cspell.with({
+        -- extra_args = {
+        --     '--config',
+        --     '~/.config/cspell/cspell.json',
+        -- },
+        diagnostics_postprocess = function(diagnostic)
+            -- レベルをINFOに変更
+            diagnostic.severity =  vim.diagnostic.severity["INFO"]
+        end,
+        -- condition = function()
+        --     -- cpellが実行できるときのみ実行
+        --     return vim.fn.executable('cspell') > 0
+        -- end,
+    }),
     -- Formatting
     null_ls.builtins.formatting.isort.with({
         command = 'python3 -m isort',
         prefer_local = '.venv/bin',
-        -- diagnostics_postprocess = function(diagnostic)
-        --     -- レベルをWARNに変更
-        --     diagnostic.severity =  vim.diagnostic.severity["WARN"]
-        -- end,
+        diagnostics_postprocess = function(diagnostic)
+            -- レベルをWARNに変更
+            diagnostic.severity =  vim.diagnostic.severity["WARN"]
+        end,
     }),
     null_ls.builtins.formatting.djlint.with({
         command = 'python3 -m djlint',
         prefer_local = '.venv/bin',
-        -- diagnostics_postprocess = function(diagnostic)
-        --     -- レベルをWARNに変更
-        --     diagnostic.severity =  vim.diagnostic.severity["WARN"]
-        -- end,
+        diagnostics_postprocess = function(diagnostic)
+            -- レベルをWARNに変更
+            diagnostic.severity =  vim.diagnostic.severity["WARN"]
+        end,
     }),
 }
 
