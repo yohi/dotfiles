ef5d57c5f1460e69aa6acae82ce859767e3e0540
Yusuke Ohi <y.ohi@diamondhead.tech>
2022-10-06 16:32:53 +0900

    commit

--- vim/lua/config/docker.lua
+++ vim/lua/config/docker.lua
@@ -1,6 +1,9 @@
 local function export_docker_environment()
     print('export docker environment')
+    -- print(os.execute('docker compose config --format json | jq ".services.app.environment" > /dev/null'))
         local handle = io.popen('docker compose config --format json | jq ".services.app.environment"')
+        -- local handle = io.popen('docker compose config --format json | jq ".services.app.environment" > /dev/null')
+        -- print(handle)
         if handle ~= nil then
             local result = handle:read("*a")
             local docker_env = vim.fn.json_decode(result)
@@ -8,5 +11,6 @@ local function export_docker_environment()
                 vim.fn.setenv(key, value)
             end
         end
+    -- end
 end
 pcall(export_docker_environment)
