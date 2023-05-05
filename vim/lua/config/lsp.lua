198af936142a720cd24771b671da7c1d308e3d00
Yusuke Ohi <yohi.consadole12@gmail.com>
2023-05-05 23:49:29 +0900

    nvim-navic

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -95,6 +95,8 @@ local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
 local mason_tool_installer = require('mason-tool-installer')
+local nvim_navic = require('nvim-navic')
+local barbecue = require('barbecue')
 
 -- local root_dir = lspconfig.util.root_pattern('.venv')
 -- local python_path = lspconfig.util.path.join(root_dir, '.venv', 'bin', 'python')
@@ -321,6 +323,109 @@ mason_lspconfig.setup_handlers({
             },
         }
     end,
+})
+
+nvim_navic.setup {
+    -- icons = {
+    --     File          = " ",
+    --     Module        = " ",
+    --     Namespace     = " ",
+    --     Package       = " ",
+    --     Class         = " ",
+    --     Method        = " ",
+    --     Property      = " ",
+    --     Field         = " ",
+    --     Constructor   = " ",
+    --     Enum          = "練",
+    --     Interface     = "練",
+    --     Function      = " ",
+    --     Variable      = " ",
+    --     Constant      = " ",
+    --     String        = " ",
+    --     Number        = " ",
+    --     Boolean       = "◩ ",
+    --     Array         = " ",
+    --     Object        = " ",
+    --     Key           = " ",
+    --     Null          = "ﳠ ",
+    --     EnumMember    = " ",
+    --     Struct        = " ",
+    --     Event         = " ",
+    --     Operator      = " ",
+    --     TypeParameter = " ",
+    -- },
+    -- icons = {
+    --   File = ' ',
+    --   Module = ' ',
+    --   Namespace = ' ',
+    --   Package = ' ',
+    --   Class = ' ',
+    --   Method = ' ',
+    --   Property = ' ',
+    --   Field = ' ',
+    --   Constructor = ' ',
+    --   Enum = ' ',
+    --   Interface = ' ',
+    --   Function = ' ',
+    --   Variable = ' ',
+    --   Constant = ' ',
+    --   String = ' ',
+    --   Number = ' ',
+    --   Boolean = ' ',
+    --   Array = ' ',
+    --   Object = ' ',
+    --   Key = ' ',
+    --   Null = ' ',
+    --   EnumMember = ' ',
+    --   Struct = ' ',
+    --   Event = ' ',
+    --   Operator = ' ',
+    --   TypeParameter = ' ',
+    -- },
+  lsp = {
+    auto_attach = true,
+    preference = {
+        'pyright',
+    },
+  },
+  highlight = false,
+  separator = " > ",
+  depth_limit = 9,
+  depth_limit_indicator = "..",
+  safe_output = true,
+  click = false
+}
+vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+
+-- triggers CursorHold event faster
+vim.opt.updatetime = 200
+
+barbecue.setup({
+  create_autocmd = false, -- prevent barbecue from updating itself automatically
+  separator = "  ",
+  icons_enabled = true,
+  icons = {
+    default = "",
+    symlink = "",
+    git = "",
+    folder = "",
+    ["folder-open"] = "",
+  },
+})
+
+vim.api.nvim_create_autocmd({
+  "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+  "BufWinEnter",
+  "CursorHold",
+  "InsertLeave",
+
+  -- include this if you have set `show_modified` to `true`
+  "BufModifiedSet",
+}, {
+  group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+  callback = function()
+    require("barbecue.ui").update()
+  end,
 })
 
 -- mason_lspconfig.setup_handlers({ function(server_name)
