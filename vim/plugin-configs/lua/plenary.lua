6bbf8ef4c13a73887885adf52d958ef78ca0f73e
Yusuke Ohi <y.ohi@diamondhead.tech>
2023-12-22 20:34:26 +0900

    commit

--- vim/plugin-configs/lua/plenary.lua
+++ vim/plugin-configs/lua/plenary.lua
@@ -1,18 +1,18 @@
 -- lua_add {{{
 
-local a = require "plenary.async"
+local plenary_async = require('plenary.async')
 
 local read_file = function(path)
-    local err, fd = a.uv.fs_open(path, "r", 438)
+    local err, fd = plenary_async.uv.fs_open(path, "r", 438)
     assert(not err, err)
 
-    local err, stat = a.uv.fs_fstat(fd)
+    local err, stat = plenary_async.uv.fs_fstat(fd)
     assert(not err, err)
 
-    local err, data = a.uv.fs_read(fd, stat.size, 0)
+    local err, data = plenary_async.uv.fs_read(fd, stat.size, 0)
     assert(not err, err)
 
-    local err = a.uv.fs_close(fd)
+    local err = plenary_async.uv.fs_close(fd)
     assert(not err, err)
 
     return data
