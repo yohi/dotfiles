ab2bd3fdf8b19b5c7a936ce5047dfe93f097c4af
Yusuke Ohi <yohi.consadole12@gmail.com>
2024-07-12 18:08:23 +0900

    commit

--- vim/dpp/hooks/lua/lsp.lua
+++ vim/dpp/hooks/lua/lsp.lua
@@ -100,29 +100,8 @@ mason.setup({
     }
 })
 
-local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-    vim.fn.sign_define(
-        sign.name,
-        {
-            texthl = sign.name,
-            text = sign.text,
-            numhl = "",
-        }
-    )
-end
-
 local config = {
     virtual_text = false,
-    signs = {
-        active = signs,
-    },
     update_in_insert = false,
     underline = true,
     severity_sort = true,
@@ -136,6 +115,14 @@ local config = {
     },
     inlay_hints = {
         enabled = false,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        },
     },
 }
 
